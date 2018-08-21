1c72845b4509889b4e7dcf3da75c6aff853316de
ranjithakalur <ranjithakalurkk@gmail.com>
2018-08-21 08:55:47 +0530

    mouse controls

--- index.js
+++ index.js
@@ -156,6 +156,7 @@ setInterval(draw, count);
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
+document.addEventListener("mousemove", mouseMoveHandler, false);
 
   function keyDownHandler(e){
     if(e.keyCode == 39){
@@ -172,6 +173,11 @@ document.addEventListener("keyup", keyUpHandler, false);
     else if(e.keyCode == 37){
       leftPressed = false;
           }
+  }
+
+  function mouseMoveHandler(e){
+    var relativeX = e.clientX - canvas.offsetLeft;
+    if(relativeX > 0 && relativeX < canvas.width) { paddleX = relativeX - paddleWidth/2; } 
   }
   //d();
 
